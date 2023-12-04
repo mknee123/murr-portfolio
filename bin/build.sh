0aa9d4f7fc8a592b08d0575bd8e27076924b238f
Miranda K <mknee@ghadv.com>
2023-12-04 11:24:54 -0500

    Remove redis from services and add wordmove. Prep for setup script.

--- bin/build.sh
+++ bin/build.sh
@@ -5,13 +5,13 @@
 # Usage: ./bin/build.sh <tag> --build-arg KEY=VALUE
 # Example: ./bin/build.sh manual-release --build-ARG ACF_PRO_KEY=<key> --build-arg FONTAWESOME_NPM_AUTH_TOKEN=<key>
 
-ECR_URI=557798128580.dkr.ecr.us-east-1.amazonaws.com
-ECR_IMAGE=murr-website
+# ECR_URI=557798128580.dkr.ecr.us-east-1.amazonaws.com
+# ECR_IMAGE=murr-website
 
-ECR_TAG="${ECR_IMAGE}:${1:-latest}"
-ECR_FULL_IMAGE="${ECR_URI}/${ECR_TAG}"
+# ECR_TAG="${ECR_IMAGE}:${1:-latest}"
+# ECR_FULL_IMAGE="${ECR_URI}/${ECR_TAG}"
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URI
-docker build -t $ECR_TAG "${@:2}" ./code
-docker tag $ECR_TAG $ECR_FULL_IMAGE
-docker push $ECR_FULL_IMAGE
+# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URI
+# docker build -t $ECR_TAG "${@:2}" ./code
+# docker tag $ECR_TAG $ECR_FULL_IMAGE
+# docker push $ECR_FULL_IMAGE
--- bin/cli.sh
+++ bin/cli.sh
@@ -2,7 +2,7 @@
 
 # OSX Utility script - Runs a Docker CLI service
 # Usage: ./bin/cli.sh <service> ...
-# Example: ./bin/cli.sh node yarn run watch
-# Example: ./bin/cli.sh node yarn run build
+# Example: ./bin/cli.sh node npm run watch
+# Example: ./bin/cli.sh node npm run build
 
 docker compose -f cli.yml run --rm $@
